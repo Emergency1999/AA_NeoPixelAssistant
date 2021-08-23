8149a8cf86e9b9bba7bb0d496dbc19e887ec6fda
Emergency1999 <johannes@schmid-company.de>
2021-08-23 02:11:38 +0200

    refactoring PixelAssistant

--- include/NeoPixelAssistant.h
+++ include/NeoPixelAssistant.h
@@ -6,54 +6,63 @@
 #include <Assistant.h>
 #include <Timer.h>
 
-typedef struct RGB {
-    byte R;
-    byte G;
-    byte B;
-    bool operator==(RGB other) {
-        return R == other.R && G == other.G && B == other.B;
-    }
-    bool operator!=(RGB other) { return !(operator==(other)); }
+struct RGB {
+    byte R, G, B;
+    bool operator==(RGB other);
+    bool operator!=(RGB other);
 };
 
-typedef struct RGBW {
-    byte R;
-    byte G;
-    byte B;
-    byte W;
-    RGBW(RGB &other) {
-        R = other.R;
-        G = other.G;
-        B = other.B;
-        W = 0;
-    };
-    RGBW(byte R, byte G, byte B, byte W) {
-        this->R = R;
-        this->G = G;
-        this->B = B;
-        this->W = W;
-    };
-    bool operator==(RGBW other) {
-        return R == other.R && G == other.G && B == other.B && W == other.W;
-    }
-    bool operator!=(RGBW other) { return !(operator==(other)); }
+struct RGBW {
+    byte R, G, B, W;
+    RGBW();
+    RGBW(RGB &other);
+    RGBW(byte R, byte G, byte B, byte W);
+    bool operator==(RGBW other);
+    bool operator!=(RGBW other);
 };
 
-class NeoPixelAssistant : public Assistant {
+class PixelAssistant : public Assistant {
+   public:
+    PixelAssistant();
+    virtual void update(){};
+
+    virtual RGBW get(uint16_t n) = 0;
+    virtual void set(RGBW color, uint16_t n) = 0;
+
+    virtual void set(RGBW color);
+    virtual void set(RGBW color, uint16_t n1, uint16_t n2);
+    virtual void setPercent(RGBW color, float p1, float p2);
+
+    virtual uint16_t count() = 0;
+};
+
+class PartPixelAssistant : public PixelAssistant {
+   public:
+    PartPixelAssistant(PixelAssistant *strip);
+
+    RGBW get(uint16_t n) override;
+    void set(RGBW color, uint16_t n) override;
+    uint16_t count() override;
+
+   private:
+    void _setup() override;
+    void _loop() override;
+    PixelAssistant *_strip;
+    uint16_t _first, _count;
+};
+
+class NeoPixelAssistant : public PixelAssistant {
    public:
     NeoPixelAssistant(Adafruit_NeoPixel *strip);
     void update();
 
     RGBW get(uint16_t n);
-    void set(RGBW color);
     void set(RGBW color, uint16_t n);
-    void set(RGBW color, uint16_t n1, uint16_t n2);
-    void setPercent(RGBW color, float p1, float p2);
-    uint16_t count();  //returns pins count
+    uint16_t count();
 
-    void setHue(uint16_t hue);
-    void setHue(uint16_t hue, uint16_t n);
-    void setHue(uint16_t hue, uint16_t n1, uint16_t n2);
+    // void setHue(uint16_t hue);
+    // void setHue(uint16_t hue, uint16_t n);
+    // void setHue(uint16_t hue, uint16_t n1, uint16_t n2);
 
    private:
     void _setup() override;
--- src/NeoPixelAssistant.cpp
+++ src/NeoPixelAssistant.cpp
@@ -1,5 +1,87 @@
 #include <NeoPixelAssistant.h>
 
+// -------------------------------------------------------------------------------- RGB
+bool RGB::operator==(RGB other) {
+    return R == other.R && G == other.G && B == other.B;
+}
+bool RGB::operator!=(RGB other) {
+    return !(operator==(other));
+}
+
+// -------------------------------------------------------------------------------- RGBW
+RGBW::RGBW() {
+    R = 0;
+    G = 0;
+    B = 0;
+    W = 0;
+}
+RGBW::RGBW(RGB& other) {
+    R = other.R;
+    G = other.G;
+    B = other.B;
+    W = 0;
+};
+RGBW::RGBW(byte R, byte G, byte B, byte W) {
+    this->R = R;
+    this->G = G;
+    this->B = B;
+    this->W = W;
+};
+bool RGBW::operator==(RGBW other) {
+    return R == other.R && G == other.G && B == other.B && W == other.W;
+}
+bool RGBW::operator!=(RGBW other) {
+    return !(operator==(other));
+}
+
+// -------------------------------------------------------------------------------- PixelAssistant
+
+void PixelAssistant::set(RGBW color) {
+    set(color, 0, count());
+}
+void PixelAssistant::set(RGBW color, uint16_t n1, uint16_t n2) {
+    for (uint16_t i = n1; i < n2; i++) {
+        set(color, i);
+    }
+}
+
+void PixelAssistant::setPercent(RGBW color, float p1, float p2) {
+    if (p2 < p1) {
+        float pt = p2;
+        p2 = p1;
+        p1 = pt;
+    }
+    if (p1 < 0) p1 = 0;
+    if (p2 > 1) p2 = 1;
+    uint16_t n1 = count() * p1;
+    uint16_t n2 = count() * p2;
+    set(color, n1, n2);
+}
+
+// -------------------------------------------------------------------------------- PartPixelAssistant
+PartPixelAssistant::PartPixelAssistant(PixelAssistant* strip) {
+    _strip = strip;
+}
+
+void PartPixelAssistant::_setup() {}
+
+void PartPixelAssistant::_loop() {}
+
+uint16_t PartPixelAssistant::count() {
+    return _count;
+}
+
+RGBW PartPixelAssistant::get(uint16_t n) {
+    ASSERT_WARN(n < count(), "n<count()", n = 0);
+    return _strip->get(n);
+}
+
+void PartPixelAssistant::set(RGBW color, uint16_t n) {
+    ASSERT_WARN(n < count(), "n<count()", n = 0);
+    return _strip->set(color, n);
+}
+
+// -------------------------------------------------------------------------------- NeoPixelAssistant
 NeoPixelAssistant::NeoPixelAssistant(Adafruit_NeoPixel* strip) {
     _strip = strip;
 }
@@ -25,21 +107,9 @@ uint16_t NeoPixelAssistant::count() {
     return _strip->numPixels();
 }
 
-void NeoPixelAssistant::setPercent(RGBW color, float p1, float p2) {
-    if (p2 < p1) {
-        float pt = p2;
-        p2 = p1;
-        p1 = pt;
-    }
-    if (p1 < 0) p1 = 0;
-    if (p2 > 1) p2 = 1;
-    uint16_t n1 = _strip->numPixels() * p1;
-    uint16_t n2 = _strip->numPixels() * p2;
-    set(color, n1, n2);
-}
-
 RGBW NeoPixelAssistant::get(uint16_t n) {
-    union COLOR {
+    ASSERT_WARN(n < count(), "n<count()", n = 0);
+    union {
         uint32_t value;
         struct {
             byte B;
@@ -47,34 +117,26 @@ RGBW NeoPixelAssistant::get(uint16_t n) {
             byte R;
             byte W;
         } byte;
-    };
-    COLOR r;
+    } r;
     r.value = _strip->getPixelColor(n);
     return {r.byte.R, r.byte.G, r.byte.B, r.byte.W};
 }
 
-void NeoPixelAssistant::set(RGBW color) {
-    set(color, 0, _strip->numPixels());
-}
 void NeoPixelAssistant::set(RGBW color, uint16_t n) {
+    ASSERT_WARN(n < count(), "n<count()", n = 0);
     _strip->setPixelColor(n, color.R, color.G, color.B, color.W);
     changed = true;
 }
-void NeoPixelAssistant::set(RGBW color, uint16_t n1, uint16_t n2) {
-    for (uint16_t i = n1; i < n2; i++) {
-        set(color, i);
-    }
-}
 
-void NeoPixelAssistant::setHue(uint16_t hue) {
-    setHue(hue, 0, _strip->numPixels());
-}
-void NeoPixelAssistant::setHue(uint16_t hue, uint16_t n) {
-    _strip->setPixelColor(n, _strip->gamma32(_strip->ColorHSV(hue)));
-    changed = true;
-}
-void NeoPixelAssistant::setHue(uint16_t hue, uint16_t n1, uint16_t n2) {
-    for (uint16_t i = n1; i < n2; i++) {
-        setHue(hue, i);
-    }
-}
+// void NeoPixelAssistant::setHue(uint16_t hue) {
+//     setHue(hue, 0, _strip->numPixels());
+// }
+// void NeoPixelAssistant::setHue(uint16_t hue, uint16_t n) {
+//     _strip->setPixelColor(n, _strip->gamma32(_strip->ColorHSV(hue)));
+//     changed = true;
+// }
+// void NeoPixelAssistant::setHue(uint16_t hue, uint16_t n1, uint16_t n2) {
+//     for (uint16_t i = n1; i < n2; i++) {
+//         setHue(hue, i);
+//     }
+// }
